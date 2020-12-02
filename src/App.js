b1cdeb7e4a557f6c04fe1204d932b083f0cfb3ca
Omi Gibba <oumie.gibba@gmail.com>
2020-12-02 16:25:39 +0100

    Button component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from "react";
+import { Button } from "./components";
+import { ReactComponent as PlusIcon } from "./icons/plus-icon.svg";
+import { ReactComponent as MinusIcon } from "./icons/minus-icon.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Hello, World!!! </h1>
+        <Button  buttonText="- Decrement"><MinusIcon /> Decrement</Button>
+        <Button primary buttonText="+ Increment"><PlusIcon /> Increment</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Button (props) {
+   return (
+      <CounterButton type="button" primary={props.primary} aria-label={props.ariaLabel} onClick={props.onClick}>{props.children}</CounterButton>
+   )
+}
+
+const CounterButton = styled.button`
+   width: 150px;
+   background-color: ${props => (props.primary ? 'hotpink' : 'white')};
+   color: ${props => (props.primary ? 'white' : 'black')};
+   text-transform: uppercase;
+   cursor: pointer;
+   padding: 0.5rem;
+   border: ${props => (props.primary ? '1px solid hotpink' : '1px solid black')};
+   border-radius: 25px;
+   transition: all 0.25s;
+   svg {
+      fill: ${props => (props.primary ? 'white' : 'black')};
+      transition: all 0.25s;
+   }
+
+   &:hover {
+      color: white;
+      background-color: ${props => (props.primary ? 'rebeccapurple' : 'black')};
+      border: ${props => (props.primary ? '1px solid rebeccapurple' : '1px solid black')};
+      svg {
+         fill: white;
+      }
+   }
+`;
+
+export default Button;
